0d9c1b484cc14c90f9acc5e205159cb6d2193f7c
josejisin@gmail.com <josejisin@gmail.com>
2024-07-08 14:10:25 +0100

    refactor: create configs package to store command texts

---
+++ configs/configs.go
@@ -0,0 +1,20 @@
+// (c) Jisin0
+
+package configs
+
+var (
+	StartText = `
+<i><b>👋 Hey there <tg-spoiler>%s</tg-spoiler>,</b>
+This bot can search multiple movie databases to get details about any movie or show.</i>
+
+<blockquote expandable><u><b>DISCLAIMER</b></u>
+
+<b>IMDb:</b> Public, commercial, and/or non-private use of the IMDb data provided by this bot is not allowed.
+This bot is only for limited non-commercial use of <a href='https://help.imdb.com/article/imdb/general-information/can-i-use-imdb-data-in-my-software/G5JTRESSHJBBHTGX'>IMDb data</a>.
+
+<b>OMDb:</b> Data distributed by the omdbapi is licensed under the <a href='https://creativecommons.org/licenses/by-nc/4.0/'>CreativeCommons 4.0 License</a> and is free to share, copy, remix, transform and build upon for any Non-Commercial application.
+
+<b>JustWatch:</b> <a href='https://support.justwatch.com/hc/en-us/articles/9567105189405-JustWatch-s-Terms-of-Use#h_01HM8Z0MS8WT2S38ND9KNEJY0Y'>Privacy Policy</a> states not to modify, copy, reverse engineer, reverse assemble or otherwise attempt to discover any source code in the Website, or to frame, scrape, rent, lease, loan, sell, assign, sublicense, distribute or create derivative works based on, or reproduce, display, publicly perform, or otherwise use the Service Content in any way for any public or commercial purpose.
+</blockquote>
+`
+)
